1c2170003a210b4cdb3221cfa90a3a49a6a78861
Noha Shehab <nshehab@iti.gov.eg>
2023-09-26 14:27:05 +0300

    creating first django app

---
+++ iti/students/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# views --> functions
+
+# function accepts http request and returns http response
+def helloworld(request):
+    return HttpResponse('Hello world')
+
+
+
+def welcome(request):
+    return HttpResponse('<h1 style="color:red">  Welcome to Django </h1>')
+
+
+def sayhi(request, username):
+    return HttpResponse(f'<h1 style="color:purple">  Hi {username} </h1>')
+

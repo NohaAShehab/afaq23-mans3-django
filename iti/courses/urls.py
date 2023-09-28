7c1e6c2daf26f2b61b924abbcba3a6263fc1ec12
Noha Shehab <nshehab@iti.gov.eg>
2023-09-28 11:25:38 +0300

    submit course to the database

--- iti/courses/urls.py
+++ iti/courses/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from courses.views import index, show, delete
+from courses.views import index, show, delete, create
 urlpatterns = [
 
     path('', index, name='courses.home'),
     path('<int:course_id>', show, name='courses.show'),
-    path('<int:course_id>/delete', delete, name='courses.delete')
+    path('<int:course_id>/delete', delete, name='courses.delete'),
+    path('create' ,create, name='courses.create')
 
 ]
--- iti/courses/views.py
+++ iti/courses/views.py
@@ -27,3 +27,21 @@ def delete(request, course_id):
     bact_to_url = reverse('courses.home')
     return redirect(bact_to_url)
 
+
+
+def create(request):
+    print(request)
+    if request.method == 'POST':
+        request_data = request.POST
+        # print(request_data)
+        name = request_data['name']
+        image = request_data['image']
+        max_grade = request_data['max_grade']
+        # print(name, image, max_grade)
+        course = Course(name=name, max_grade=max_grade, image=image)
+        course.save()
+
+        # return HttpResponse("post request accepted")
+        bact_to_url = reverse('courses.home')
+        return redirect(bact_to_url)
+    return render(request, 'courses/create.html')
